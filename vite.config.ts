3bbcfd38be17902b4e9a4dfd65ea3eecf9477cc1
goett <goett@users.noreply.github.com>
2026-03-07 13:26:52 +0100

    chore: release v1.1.29

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     outDir: 'dist',
   },
   define: {
-    'process.env.APP_VERSION': JSON.stringify("1.1.28"),
+    'process.env.APP_VERSION': JSON.stringify("1.1.29"),
   },
   server: {
     port: 3000,
