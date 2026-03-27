7c2b15e97e3459652b9164c741913d262e2b0fe3
bieblbriefumschlag <bieblbriefumschlag@users.noreply.github.com>
2026-03-27 23:27:56 +0100

    chore: release v1.1.36

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     outDir: 'dist',
   },
   define: {
-    'process.env.APP_VERSION': JSON.stringify("1.1.32"),
+    'process.env.APP_VERSION': JSON.stringify("1.1.36"),
   },
   server: {
     port: 3000,
